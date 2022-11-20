a790213b3bf8e8be95e16484d382b697e6d066ed
Moza Sajidah <moza.sajidah@gmail.com>
2022-11-20 22:48:17 +0300

    Push Buyer & Seller Class

--- tubesPBO/src/tubesPBO/Buyer.java
+++ tubesPBO/src/tubesPBO/Buyer.java
@@ -1,5 +1,22 @@
 package tubesPBO;
 
 public class Buyer {
-
+    private String paymentMethod;
+    private int creditCard;
+    
+    public void setPaymentMethod (String paymentMethod){
+        this.paymentMethod = paymentMethod;
+    }
+    
+    public String getPaymentMethod (){
+        return this.paymentMethod;
+    }
+    
+    public void setCreditCard (int creditCard){
+        this.creditCard = creditCard;
+    }
+    
+    public int getCreditCard (){
+        return this.creditCard;
+    }
 }
--- tubesPBO/src/tubesPBO/Seller.java
+++ tubesPBO/src/tubesPBO/Seller.java
@@ -1,5 +1,30 @@
 package tubesPBO;
 
 public class Seller {
-
+    private String namaToko;
+    private Listing produkListing;
+    
+    public void setNamaToko (String namaToko){
+        this.namaToko = namaToko;
+    }
+    
+    public String getNamaToko(){
+        return this.namaToko;   
+    }
+    
+    public void setProdukListing (Listing produkListing){
+        this.produkListing = produkListing;
+    }
+    
+    public Listing getProdukListing(){
+        return this.produkListing;
+    }
+    
+    public void addProduct(){
+    
+    }
+    
+    public void removeProduct(){
+    
+    }
 }
