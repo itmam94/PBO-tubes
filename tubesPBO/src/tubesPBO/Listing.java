b9a757901b5bb82f0d327634429fce904cd01482
mthariqfaturrahman <mthariqfaturrahman@student.telkomuniversity.ac.id>
2022-11-20 23:18:47 +0700

    Listing

--- tubesPBO/src/tubesPBO/Listing.java
+++ tubesPBO/src/tubesPBO/Listing.java
@@ -1,5 +1,44 @@
 package tubesPBO;
 
 public class Listing {
+    private final Product produk;
+    private int quantity, category;
 
+    public Listing(){
+
+    }
+
+    public void setProduk(Product produk) {
+        this.produk = produk;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduk() {
+        return produk;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void addQuantity(int add) {
+        this.quantity += add;
+    }
+
+    public void removeQuantity(int remove) {
+        this.quantity -= remove;
+    }
 }
+
+
