4da2a1be45e113d4f14a15ad7be695fe8221a631
itmam <itmam@RSD-L340>
2022-11-20 22:42:46 +0700

    Address dan Account DONE

--- tubesPBO/src/tubesPBO/Account.java
+++ tubesPBO/src/tubesPBO/Account.java
@@ -1,5 +1,85 @@
 package tubesPBO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
+	
+	private String userID, email, password, name, phoneNumber;
+	private List<Address> addressList =new ArrayList<Address>(); 
+	
+	public Account() {
+	}
+	
+	public void register(String email, String password, String name, String phoneNumber) {
+		setEmail(email);
+		setPassword(password);
+		setName(name);
+		setPhoneNumber(phoneNumber);
+	}
+	
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public boolean login(String email, String password) {
+		if (email.equalsIgnoreCase(this.email) && (password.equals(this.password))) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void addAddress(Address address) {
+		addressList.add(address);
+	}
+	
+	public void removeAddress(Address address) {
+		addressList.remove(address);
+	}
+	
+	public void printAddress() {
+		for (Address address : addressList) {
+			System.out.println(address.getName() + ": " + address.getStreet() + " " +
+		address.getCity() + " " + address.getProvice() + " " + address.getPostalCode() +
+		" " + address.getCountry());
+		}
+	}
 
 }
--- tubesPBO/src/tubesPBO/Address.java
+++ tubesPBO/src/tubesPBO/Address.java
@@ -1,5 +1,69 @@
 package tubesPBO;
 
 public class Address {
+	
+	private String name, street, city, provice, country;
+	private int postalCode;
+	
+	public Address() {
+	}
+
+	public Address(String name, String street, String city, String provice, String country, int postalCode) {
+		super();
+		this.name = name;
+		this.street = street;
+		this.city = city;
+		this.provice = provice;
+		this.country = country;
+		this.postalCode = postalCode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getProvice() {
+		return provice;
+	}
+
+	public void setProvice(String provice) {
+		this.provice = provice;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(int postalCode) {
+		this.postalCode = postalCode;
+	}
 
 }
